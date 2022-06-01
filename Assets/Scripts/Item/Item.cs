d3ce27d463379d035aea9c9a8e057f05f2151537
David Jiang <djiang86@binghamton.edu>
2022-05-31 23:05:06 -0400

    Automatically Attach Item Nudge To Reapable Scenary

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -36,6 +36,16 @@ public class Item : MonoBehaviour
     }
     public void Init(int itemCodeParam)
     {
-        
+        if (itemCodeParam != 0)
+        {
+            ItemCode = itemCodeParam;
+            ItemDetails itemDetails = InventoryManager.Instance.GetItemDetails(ItemCode);
+            spriteRenderer.sprite = itemDetails.itemSprite;
+
+            if (itemDetails.itemType == ItemType.Reapable_scenary)
+            {
+                gameObject.AddComponent<ItemNudge>();
+            }
+        }
     }
 }

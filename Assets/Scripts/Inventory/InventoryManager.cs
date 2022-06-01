1e28ebe748f0530ebde7f787d4acd78c245c738f
David Jiang <djiang86@binghamton.edu>
2022-05-31 21:26:09 -0400

    Inventory Manager

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : SingletonMonobehaviour<InventoryManager>
+{
+    private Dictionary<int, ItemDetails> itemDetailsDictionary;
+
+    [SerializeField] private SO_ItemList itemList = null;
+
+    private void Start()
+    {
+        CreateItemDetailsDictionary();
+    }
+
+    // Populate the dictionary from the scriptable object item list
+    private void CreateItemDetailsDictionary()
+    {
+        itemDetailsDictionary = new Dictionary<int, ItemDetails>();
+
+        foreach (ItemDetails itemDetail in itemList.itemDetails)
+        {
+            itemDetailsDictionary.Add(itemDetail.itemCode, itemDetail);
+        }
+    }
+
+    public ItemDetails GetItemDetails(int itemCode)
+    {
+        ItemDetails itemDetails;
+
+        if (itemDetailsDictionary.TryGetValue(itemCode, out itemDetails)) {
+            return itemDetails;
+        } else
+        {
+            return null;
+        }
+    }
+}

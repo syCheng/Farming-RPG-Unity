970a1a2716647bf44b3d25e74b44e291fc692d80
“sycheng” <sycheng@pisx.com>
2024-08-06 11:48:00 +0800

    Modify

---
+++ Assets/RPGPractice2/Scripts/Enums/Enums.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FarmingRPG2
+{
+
+    public enum CharacterPartAnimator
+    {
+        body,
+        arms,
+        hair,
+        tool,
+        hat,
+        count
+    }
+
+    public enum Direction
+    {
+        up,
+        down,
+        left,
+        right,
+        none,
+    }
+}

9edf4f707b61ba0b8d527f82225fb3cb8e3f8403
“sycheng” <sycheng@pisx.com>
2024-08-21 11:02:48 +0800

    UI InventoryBar Update UI InventorySlots

--- Assets/RPGPractice2/Scripts/UI/UI Inventory/UIInventoryBar.cs
+++ Assets/RPGPractice2/Scripts/UI/UI Inventory/UIInventoryBar.cs
@@ -1,3 +1,4 @@
+using FarmingRPG;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,6 +12,9 @@ namespace FarmingRPG2
     {
 
         [SerializeField] private Sprite blank16x16Sprite = null;
+
+
+        [SerializeField] private UIInventorySlot[] uiInventorySlots;
 
 
         private RectTransform rectTransform;
@@ -29,6 +33,73 @@ namespace FarmingRPG2
         {
             rectTransform = GetComponent<RectTransform>();
         }
+
+
+        private void OnEnable()
+        {
+            EventHandler.InventoryUpdatedEvent += EventHandler_InventoryUpdatedEvent;
+        }
+
+       
+
+        private void OnDisable()
+        {
+            EventHandler.InventoryUpdatedEvent -= EventHandler_InventoryUpdatedEvent;
+        }
+
+
+
+        private void EventHandler_InventoryUpdatedEvent(InventoryLocation inventoryLocation, List<InventoryItem> inventoryList)
+        {
+            if (inventoryLocation == InventoryLocation.player)
+            {
+                //清除所有Slots
+                ClearInventorySlots();
+
+                if (uiInventorySlots.Length > 0 && inventoryList.Count > 0)
+                {
+                    for (int i = 0; i < uiInventorySlots.Length; i++)
+                    {
+                        if (i < inventoryList.Count)
+                        {
+                            int itemCode = inventoryList[i].itemCode;
+
+                            ItemDetails itemDetails = InventoryManager.Instance.GetItemDetails(itemCode);
+
+                            if (itemDetails != null)
+                            {
+                                uiInventorySlots[i].inventorySlotImage.sprite = itemDetails.itemSprite;
+                                uiInventorySlots[i].textMeshProUGUI.text = inventoryList[i].itemQuantity.ToString();
+                                uiInventorySlots[i].itemDetails = itemDetails;
+                                uiInventorySlots[i].itemQuantity = inventoryList[i].itemQuantity;
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ClearInventorySlots()
+        {
+            if (uiInventorySlots.Length > 0)
+            {
+                // loop through inventory slots and update with blank sprite
+                for (int i = 0; i < uiInventorySlots.Length; i++)
+
+                {
+                    uiInventorySlots[i].inventorySlotImage.sprite = blank16x16Sprite;
+                    uiInventorySlots[i].textMeshProUGUI.text = "";
+                    uiInventorySlots[i].itemDetails = null;
+                    uiInventorySlots[i].itemQuantity = 0;
+                }
+            }
+        }
+
+
 
         // Update is called once per frame
         void Update()
